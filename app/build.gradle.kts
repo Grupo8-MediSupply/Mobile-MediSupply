97bf12da8513e342bdde6a6165165dd370d6532e
Edgar Melara <e.melaral@uniandes.edu.co>
2025-11-01 16:01:00 -0600

    feat: actualizar a java 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,12 +35,12 @@ android {
     }
     
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
 
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
     
     buildFeatures {
@@ -60,7 +60,7 @@ android {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(21)
 }
 
 dependencies {
